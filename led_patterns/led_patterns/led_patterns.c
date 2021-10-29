17c4660fda38d9ba1e6559a8dcf54461925fe789
Mustafa Muhammed <MustafaMuhammed2103@gmail.com>
2021-10-30 01:17:42 +0200

    Add files via upload

---
+++ led_patterns/led_patterns/led_patterns.c
@@ -0,0 +1,65 @@
+﻿/*
+ * led_patterns.c
+ *
+ * Created: 14/07/2020 03:08:20 ص
+ *  Author: mostafa gomaa
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+unsigned char i;
+
+void patt1(void)
+{
+	PORTA=0x01;
+	for (i=0;i<8;i++)
+	{
+		_delay_ms(500);
+		PORTA=(PORTA<<1);
+	}
+}
+
+void patt2(void)
+{
+	PORTA=0x80;
+	for (i=0;i<8;i++)
+	{
+		_delay_ms(500);
+		PORTA=(PORTA>>1);
+	}
+}
+
+void patt3(void)
+{
+	PORTA=0x01;
+	for (i=0;i<8;i++)
+	{
+		_delay_ms(500);
+		PORTA = PORTA | (PORTA<<1);
+		
+	}			
+}
+
+void patt4(void)
+{
+	PORTA=0xff;
+	for (i=0;i<8;i++)
+	{
+		_delay_ms(500);
+		PORTA=(PORTA>>1);
+	}
+}
+
+int main(void)
+{
+	DDRA=0xff;
+	PORTA=0x00;
+	
+    while(1)
+    {
+        patt1();
+		patt2();
+		patt3();
+		patt4(); 
+    }
+}
